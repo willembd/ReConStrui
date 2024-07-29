5e58c7556351b2e24b44a45def0c4eaba38f8161
LuizFloriano <luizfloriano92@gmail.com>
2024-07-28 22:36:53 -0300

    feat: atualizando os cards e o carrossel

--- src/Componentes/cards/cards.jsx
+++ src/Componentes/cards/cards.jsx
@@ -1,26 +1,58 @@
-import Rachao from "../../../public/rachao 1.svg"
-import { Botao, Caixa, Container, Im, Linha } from "./styleCards"
+
+import { Alinhamento, Botao, Caixa, Carrosel, Container, Im, Linha, Palavra, Title } from "./styleCards";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/scrollbar";
+import { Navigation, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "./card.css"
 
 
-export default function Cards(){
-    return(
+export default function Cards() {
+    const imagens = [
+        { id: '1', image: "/pedrisco 1.svg",title:"Pedrisco" },
+        { id: '2', image: "/rachao 1.svg",title:"Rachão" },
+        { id: '3', image: "/image 36areia.svg" ,title:"Areia reciclada"},
+        { id: '4', image: "/brita 1.svg" ,title:"Brita reciclada"},
+        { id: '5', image: "/pedrisco 1.svg" ,title:"Pedrisco"},
+        { id: '6', image: "/rachao 1.svg" ,title:"Rachão"},
+        { id: '7', image: "/image 36areia.svg" ,title:"Areia reciclada"},
+        { id: '8', image: "/brita 1.svg" ,title:"Brita reciclada"},
+        { id: '9', image: "/pedrisco 1.svg" ,title:"Pedrisco"},
+        { id: '10', image: "/rachao 1.svg",title:"Rachão" },
+    ];
+
+    return (
         <Container>
-            <Caixa>
-                <div className="box-item">
-                    <div className="img">
-                        <Im src={Rachao} alt="" />
-                    </div>
-                    <Linha>
-                        <h2>Rachão</h2>
-                        <p>AGR Ambiental</p>
-                    </Linha>
-                    <Container>
-                        <Botao>Compra</Botao>
-                    </Container>
-                </div>
-            </Caixa>
-        </Container>
+            <Carrosel>
+                <Swiper 
+                    modules={[Navigation, Scrollbar, A11y]}
+                    slidesPerView={5}
+                    navigation
+                >
+                    {imagens.map((item) => (
+                        <SwiperSlide key={item.id}>
+                            <Container>
+                                <Caixa>
+                                    <div className="box-item">
+                                        <div className="img">
+                                            <Im src={item.image} alt="Slide" className='slide-item'/>
+                                        </div>
+                                        <Alinhamento>
+                                            <Linha>
+                                                <Title>{item.title}</Title>
+                                                <Palavra>AGR Ambiental</Palavra>
+                                                <Botao>Compra</Botao>
+                                            </Linha>
+                                        </Alinhamento>
+                                    </div>
+                                </Caixa>
 
-        
-    )
+                            </Container>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </Carrosel>
+        </Container>
+    );
 }
--- src/Componentes/cards/styleCards.js
+++ src/Componentes/cards/styleCards.js
@@ -1,35 +1,37 @@
 import styled from "styled-components";
 
 export const Caixa = styled.div`
-    position: relative;
+    display: flex;
+    flex-direction: column;
     background-color: #fff;
-    border-radius: 12px;
-    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.5);
-    height: 18,75rem;
-    width:13,4375rem ;
+    border-radius: 0.75rem;
+    box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, 0.5);
+    height: 19rem;
+    width: 14rem;
     box-sizing: border-box;
     transition: transform 0.3s ease, box-shadow 0.3s ease;
     
     
 `
 export const Im = styled.img`
-    width: 100%;
-    height: auto;
-    border-radius: 12px;
+    width: 14rem;
+    border-radius: 0.75rem 0.75rem 0 0 ;
     
 `
 export const Botao = styled.button`
-    width: 70%;
-    padding: 12px;
+    width: 9rem;
+    padding: 0.4rem;
     background-color: #AF7503;
     color: #fff;
     border: none;
-    border-radius: 8px;
+    border-radius: 0.5rem;
     cursor: pointer;
     font-size: 1em;
     transition: background-color 0.3s ease;
-    margin: 10px;
-    font-weight: bold;
+    margin: 0.625rem;
+    font-weight: bolder;
+    font-size: 1rem;
+    margin: 0.4rem;
 `
 export const Container = styled.div`
     position: relative;
@@ -38,17 +40,28 @@ export const Container = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: 5px;
 `
 export const Linha = styled.div`
     display: flex;
     flex-direction: column;
     flex-wrap: wrap;
-    justify-content: center;
     align-items: center;
-    gap: 15px;
-    `
-    // export const Linha = styled.div`
-    // display: flex;
-    // flex-wrap: wrap;
-    // `
+    color:#1C4525 ;
+`
+export const Carrosel = styled.div`
+    height:19.011rem;
+    width: 73.581rem;
+`
+export const Alinhamento = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+export const Title = styled.h1`
+    font-size: 1rem ;
+    font-weight: bold;
+`
+export const Palavra = styled.p`
+    font-size: 0.8rem ;
+    font-weight: normal;
+    margin: 0.3rem;
+`
