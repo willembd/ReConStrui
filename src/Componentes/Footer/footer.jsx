bd9710a01dc6081ff8082c51e800685b362c2c54
Arline Monteiro <arline.monteiro@gmail.com>
2024-07-28 22:11:31 -0300

    feate: atualização do footer

--- src/Componentes/Footer/footer.jsx
+++ src/Componentes/Footer/footer.jsx
@@ -1,70 +1,64 @@
-import LogoHiper from "../../assets/logo-hipercard.png"
-import LogoMaster from "../../assets/logo-mastercard.png"
-import LogoVisa from "../../assets/logo-visa.png"
-import LogoElo from "../../assets/logo-elo.png"
-import LogoPix from "../../assets/logo-pix.png"
-import Instagram from "../../assets/logo_instagram.png"
-import Facebook from "../../assets/logo_facebook.png"
-import Whastzapp from "../../assets/logo_whatsapp.png"
-import LogoRecontrui from "../../assets/logo-recontrui.png"
+import LogoHiper from "../../assets/logo-hipercard.svg";
+import LogoMaster from "../../assets/logo-mastercard.svg";
+import LogoVisa from "../../assets/logo-visa.svg";
+import LogoElo from "../../assets/logo-elo.svg";
+import LogoPix from "../../assets/logo-pix.svg";
+import Instagram from "../../assets/logo_instagram.png";
+import Facebook from "../../assets/logo_facebook.png";
+import Whastzapp from "../../assets/logo_whatsapp.png";
+import LogoRecontrui from "../../assets/logo-recontrui.png";
 
-import * as S from "./footerStyle"
+import * as S from "./footerStyle";
 
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <S.FooterContainer>
             <S.Logo>
                 <img src={LogoRecontrui} alt="logo rescontrui" />
             </S.Logo>
 
-            <div>
-                <div>
-                    <S.Link>
-                        <h2>Link Rápidos</h2> 
-                    </S.Link>
-                    
-                    <ul>
-                        <li>
-                            <a href="">Sobre nós</a>
-                        </li>
-                        <li>
-                            <a href="">Usinas Parceiras</a>
-                        </li>
-                    </ul>
-                </div>
+            <S.FooterContato>
+                <S.Link>
+                    <h2>Link Rápidos</h2>
+                    <S.LinkContent>
+                        <a href="">Sobre nós</a>
+
+                        <a href="">Usinas Parceiras</a>
+                    </S.LinkContent>
+                </S.Link>
 
                 <S.Pagamentos>
-                    <div>
-                        <h2>Formas de Pagamentos</h2>
-                    </div>
-                    
-                    <img src={LogoHiper} alt="logo mastercard" />
-                    <img src={LogoVisa} alt="logo visa" />
-                    <img src={LogoMaster} alt="logo master" />
-                    <img src={LogoElo} alt="logo elo" />
-                    <img src={LogoPix} alt=" logo pix" />
+                    <h2>Formas de Pagamentos</h2>
+
+                    <S.Pagamentosimg>
+                        <img src={LogoHiper} alt="logo mastercard" />
+                        <img src={LogoVisa} alt="logo visa" />
+                        <img src={LogoMaster} alt="logo master" />
+                    </S.Pagamentosimg>
+                    <S.imagemcont>
+                        <img src={LogoElo} alt="logo elo" />
+                        <img src={LogoPix} alt=" logo pix" />
+                    </S.imagemcont>
                 </S.Pagamentos>
 
                 <S.Contatos>
                     <h2>Contatos</h2>
-                    <ul>
-                        <li>Email:Reconstrue@gmail.com</li>
-                        <li>Telefone:(81)3333-3333</li>
-                    </ul>
+                    <S.ContatosD>
+                        <p>Email: Reconstrue@gmail.com</p>
+                        <p>Telefone: (81)3333-3333</p>
+                    </S.ContatosD>
                 </S.Contatos>
 
                 <S.Redes>
-                    <div>
-                        <h2>Redes Sociais</h2>
-                    </div>
+                    <h2>Redes Sociais</h2>
 
-                    <div>
+                    <S.ContentRedes>
                         <img src={Instagram} alt="instagram" />
                         <img src={Whastzapp} alt="whastzapp" />
                         <img src={Facebook} alt="facebook" />
-                    </div>
+                    </S.ContentRedes>
                 </S.Redes>
-            </div>
+            </S.FooterContato>
         </S.FooterContainer>
     );
 }
--- src/Componentes/Footer/footerStyle.js
+++ src/Componentes/Footer/footerStyle.js
@@ -1,29 +1,88 @@
 import styled from "styled-components";
-
+//stilizando o footer.
 export const FooterContainer = styled.footer`
-    background-color: '#1C4525';
-    padding: 10px;
-    text-align: center;
+    background-color: #1c4525f0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 35vh;
+    color: white;
+    a {
+        color: white;
+    }
+    width: 100%;
+`;
 
-`
+//stilizando footercontato  Maior div componente onde os elementos do footer estão dentro.
+export const FooterContato = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    grid-template-columns: repeat(4, 1fr);
+    align-content: center;
+    justify-content: center;
+    gap: 60px;
+    padding: 20px;
+`;
 
+//stilizando logo Reconstrue
 export const Logo = styled.div`
-    padding: 1px;
+    padding: 30px;
+    img {
+        width: 100%;
+        max-width: 20rem;
+        margin-left: 100px;
+    }
+`;
 
-`
+//stilizando o elemento link
 export const Link = styled.div`
-    padding: 1px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`;
 
-`
+export const LinkContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+`;
+//stilizando o elemento de pagamentos imagens geral
 export const Pagamentos = styled.div`
-    padding: 1px;
-
-`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    img {
+        width: 55px;
+        height: 39px;
+    }
+`;
+//stilizando posicionalmento das imagens de pagamento (pagamentosimg e imagemcont)
+export const Pagamentosimg = styled.div`
+    display: flex;
+    justify-content: space-around;
+`;
+export const imagemcont = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+`;
 export const Contatos = styled.div`
-    padding: 1px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`;
 
-`
+export const ContatosD = styled.div``;
+//stilizando elemento redes sociais
 export const Redes = styled.div`
-    padding: 1px;
-
-`
+    display: grid;
+`;
+export const ContentRedes = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+    img {
+        width: 35px;
+        height: 35px;
+    }
+`;
