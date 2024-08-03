707143da9788718bc6a307a7d1ec584da8c26723
Wallace-Modesto <Wallace_15_modesto@hotmail.com>
2024-08-02 23:27:41 -0300

    feat: Tela (Meus Pedidos)
    Está faltando estilizar

--- src/Pages/Pedidos/pedidos.jsx
+++ src/Pages/Pedidos/pedidos.jsx
@@ -1,11 +1,137 @@
-export default function Pedidos(){
-    return (
-        <>
-        <div className="Pedidos">
-            <h1>Meus Pedidos</h1>
-        </div>
-        
-        </>
-    )
+import Cards from "../../../public/brita 1.svg";
+import Input from "../../Componentes/Input/input"
+import NavBar from "../../Componentes/NavBar/navbar"
+import Footer from "../../Componentes/Footer/footer"
+// import "./pedidosStyle"
+import * as S from "./pedidosStyle"
 
+
+
+export default function Pedidos () {
+    return (
+
+<>
+            <S.NavBar>
+                <NavBar/>
+            </S.NavBar>
+        <S.Primeiracaixa>
+
+            <S.Titulo>
+                <h1>Meus Pedidos</h1>
+            </S.Titulo>
+
+            <S.Cards>
+                <S.img src={Cards} alt="" />
+                <h1>Brita</h1>
+                <h3>AGR Ambiental</h3>
+            </S.Cards>
+
+            <S.Info>
+                        
+                <label htmlFor="quantidade">Quantidade:</label>
+                <Input type="text" id="nome" name="nome" />
+
+                <label htmlFor="nome">Rua:</label>
+                <Input type="text" id="nome" name="nome" />
+
+                <label htmlFor="nome">Cidade:</label>
+                <Input type="text" id="nome" name="nome" />
+
+                <label htmlFor="nome">Bairro:</label>
+                <Input type="text" id="nome" name="nome" />
+
+                <label htmlFor="nome">Nº</label>
+                <Input type="text" id="nome" name="nome" />
+
+                <label htmlFor="nome">Cep:</label>
+                <Input type="text" id="nome" name="nome" />
+            </S.Info>
+
+            <div className="barra de progresso">
+                <p>Aguardando Orçamento</p>
+            </div>
+
+            <div className="button">
+                <button>Cancelar</button>
+                <button>Ver Orcamento</button>
+            </div>
+        </S.Primeiracaixa>
+
+        <S.SegundaCaixa>
+            <div className="img">
+                <img src={Cards} alt="" />
+                <h1>Brita</h1>
+                <h3>AGR Ambiental</h3>
+            </div>
+
+            <div className="input">   
+                <label htmlFor="quantidade">Quantidade:</label>
+                <Input type="text" id="quantidade" name="quantidade" />
+
+                <label htmlFor="rua">Rua:</label>
+                <Input type="text" id="rua" name="rua" />
+
+                <label htmlFor="cidade">Cidade:</label>
+                <Input type="text" id="cidade" name="cidade" />
+
+                <label htmlFor="bairro">Bairro:</label>
+                <Input type="text" id="bairro" name="bairro" />
+
+                <label htmlFor="numero">Nº</label>
+                <Input type="text" id="numero" name="numero" />
+
+                <label htmlFor="cep">Cep:</label>
+                <Input type="text" id="cep" name="cep" />   
+            </div>
+
+            <div className="barra de progresso">
+                <h2>Orçamento Recebido</h2>
+            </div>
+
+            <div className="button">
+                <button>Cancelar</button>
+                <button>Ver Orcamento</button>
+            </div>
+        </S.SegundaCaixa>
+
+        <S.TerceiraCaixa>
+            <div className="img">
+                <img src={Cards} alt="" />
+                <h1>Brita</h1>
+                <h3>AGR Ambiental</h3>
+            </div>
+
+            <div className="input">   
+                <label htmlFor="quantidade">Quantidade:</label>
+                <Input type="text" id="quantidade" name="quantidade" />
+
+                <label htmlFor="rua">Rua:</label>
+                <Input type="text" id="rua" name="rua" />
+
+                <label htmlFor="cidade">Cidade:</label>
+                <Input type="text" id="cidade" name="cidade" />
+
+                <label htmlFor="bairro">Bairro:</label>
+                <Input type="text" id="bairro" name="bairro" />
+
+                <label htmlFor="numero">Nº</label>
+                <Input type="text" id="numero" name="numero" />
+
+                <label htmlFor="cep">Cep:</label>
+                <Input type="text" id="cep" name="cep" />   
+            </div>
+
+            <div className="barra de progresso">
+                <h2>Compra Realizada</h2>
+                <p>Entrega prevista: 00/00/0000</p>
+            </div>
+
+        </S.TerceiraCaixa>
+            <div className="footer">
+                <Footer/>
+            </div>
+
+</>
+
+    )
 }
---
+++ src/Pages/Pedidos/pedidosStyle.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Titulo = styled.h1`
+  text-align: center;
+  color: #2c6c35;
+  margin-bottom: 40px;
+`
+
+
+export const Primeiracaixa = styled.div `
+width: 100%;
+max-width: 1200px;
+margin: 0 auto;
+padding: 20px;
+box-sizing: border-box;
+
+`
+export const Cards = styled.img`
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  padding: 20px;
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+
+`
+
+export const SegundaCaixa = styled.div `
+ width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+  box-sizing: border-box;
+
+`
+export const TerceiraCaixa = styled.div `
+ width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+  box-sizing: border-box;
+
+`
