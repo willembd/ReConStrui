92bbc948eacb731e0245fff53c98dcf41e90abce
Willemberg Bruno <willem.bruno@hotmail.com>
2024-08-04 22:55:40 -0300

    feat: atualização style pagina cartão

--- src/Pages/PagamentoCartao/cartao.jsx
+++ src/Pages/PagamentoCartao/cartao.jsx
@@ -1,60 +1,75 @@
-import * as S from "./cartaoStyle.js"
 
-import ImgLogo from "../../assets/logoNavBar.svg";
+import * as S from "./cartaoStyle.js";
+
+import LogoBranca from "../../assets/LogoBranca.svg";
 import Input from "../../Componentes/Input/input";
 import ButtonMod from "../../Componentes/ButtonModelo/buttonModelo.jsx";
+import { useNavigate } from "react-router-dom";
+import NavBar from "../../Componentes/NavBar/navbar.jsx";
+import Footer from "../../Componentes/Footer/footer.jsx";
 import Select from "../../Componentes/Select/select.jsx";
 
-export default function Cartao() {
+export default function PaginaLogin() {
+    const navigate = useNavigate();
+
+    const handleFechar = () => {
+        navigate("/");
+    };
+
     return (
-        <S.Caixa>
-            <S.Modal>
-
-                <S.Button>X</S.Button>
-
-                <S.Imagem>
-                    <S.Logo src={ImgLogo} alt="" />
-                </S.Imagem>
-
-                <S.Linha>
-                </S.Linha>
-
-                <S.Form>
-                    <S.Titulo>Pagamento</S.Titulo>
-                    <S.DivInput>
-                        <Input text="Titular" />
-                        <Input text="CPF" />
-                        <Input text="Numero do Cartão" />
-                        <Input text="Validade" />
-                        <Input text="Código" />
-                    </S.DivInput>
+        <>
+            <NavBar />
+            <S.Caixa>
+                <S.Modal>
                     
-                    <S.DivRadio>
-                        <div>
-                            <input type="radio" value="Usuario"/>
-                            <label htmlFor="">Crédito</label>
-                        </div>
-                        <div>
-                            <input type="radio" value="Empresa"/>
-                            <label htmlFor="">Débito</label>
-                        </div>
-                    </S.DivRadio>
+                    <S.ModalContainer>
+                        <S.Imagem>
+                            <S.ButtonContainer>
+                                <S.Button onClick={handleFechar}>Voltar</S.Button>
+                            </S.ButtonContainer>
+                            <S.Logo src={LogoBranca} alt="" />
+                            <div>
 
-                    <S.DivSelect>
-                        <Select>
-                                <option value="">Parcela</option>
-                                <option value="">2x</option>
-                                <option value="">3x</option>
-                        </Select> 
-                    </S.DivSelect>
-                    
+                            </div>
+                        </S.Imagem>
 
-                    <ButtonMod text="Finalizar"/>
+                        <S.Form>
+                            <S.Titulo>Pagamento</S.Titulo>
+                            <S.DivInput>
+                                <Input text="Titular" />
+                                <Input text="CPF" />
+                                <Input text="Numero do Cartão" />
+                                <Input text="Validade" />
+                                <Input text="Código" />
+                            </S.DivInput>
+
+                            <S.FormadePagamento>
+                                <S.DivRadio>
+                                    <input type="radio" value="Usuario"/>
+                                    <label htmlFor="">Crédito</label>
+                                </S.DivRadio>
+                                <S.DivRadio>
+                                    <input type="radio" value="Empresa"/>
+                                    <label htmlFor="">Débito</label>
+                                </S.DivRadio>
+                                <S.DivSelect>
+                                    <Select>
+                                            <option value="">Parcela</option>
+                                            <option value="">2x</option>
+                                            <option value="">3x</option>
+                                    </Select> 
+                                </S.DivSelect>
+                            </S.FormadePagamento>
 
 
-                </S.Form>
-            </S.Modal>
-        </S.Caixa>
+                            <ButtonMod text="Finalizar" />
 
+                            
+                        </S.Form>
+                    </S.ModalContainer>
+                </S.Modal>
+            </S.Caixa>
+            <Footer/>
+        </>
     );
 }
