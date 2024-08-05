b21050c370ac2c632f89b1f695e69cdd4c495484
Willemberg Bruno <willem.bruno@hotmail.com>
2024-08-04 23:48:58 -0300

    feat: atualização pagina pix

--- src/Pages/PagamentoPix/pix.jsx
+++ src/Pages/PagamentoPix/pix.jsx
@@ -1,41 +1,52 @@
-import * as S from "./pixStyle.js"
+import * as S from "./pixStyle.js";
 
-import ImgLogo from "../../assets/logoNavBar.svg";
+import LogoBranca from "../../assets/LogoBranca.svg";
 import ImgPix from "../../assets/qrcodepix.svg"
 import ButtonMod from "../../Componentes/ButtonModelo/buttonModelo.jsx";
+import { useNavigate } from "react-router-dom";
 import NavBar from "../../Componentes/NavBar/navbar.jsx";
+import Footer from "../../Componentes/Footer/footer.jsx";
 
 
-export default function Pix() {
+export default function PaginaLogin() {
+    const navigate = useNavigate();
+
+    const handleFechar = () => {
+        navigate("/");
+    };
+
     return (
         <>
-            <NavBar/>
-
+            <NavBar />
             <S.Caixa>
                 <S.Modal>
+                    
+                    <S.ModalContainer>
+                        <S.Imagem>
+                            <S.ButtonContainer>
+                                <S.Button onClick={handleFechar}>Voltar</S.Button>
+                            </S.ButtonContainer>
+                            <S.Logo src={LogoBranca} alt="" />
+                            <div>
 
-                    <S.Button>X</S.Button>
+                            </div>
+                        </S.Imagem>
 
-                    <S.Imagem>
-                        <S.Logo src={ImgLogo} alt="" />
-                    </S.Imagem>
-
-                    <S.Linha>
-                    </S.Linha>
-
-                    <S.Form>
-                        <S.Titulo>Pagamento</S.Titulo>
-                        <h2>Pague com o Pix</h2>
+                        <S.Form>
+                            <S.Titulo>Pagamento</S.Titulo>
+                            <h2>Pague com o Pix</h2>
                         
-                        <img src={ImgPix} alt="" />
-                        <p>00045870acsda887124reconstrui01714</p>
-                        <ButtonMod text="Copiar"/>
+                            <img src={ImgPix} alt="" />
+                            <p>00045870acsda887124reconstrui01714</p>
 
-                    </S.Form>
+                            <ButtonMod text="Copiar" />
+    
+                        </S.Form>
+
+                    </S.ModalContainer>
                 </S.Modal>
             </S.Caixa>
-
+            <Footer/>
         </>
-
     );
 }
