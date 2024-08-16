3d71160827e11031256f6fb7d048b56efbcca006
Willemberg Bruno <willem.bruno@hotmail.com>
2024-08-15 22:40:36 -0300

    feat: autentificação login

---
+++ server/src/controllers/authControllers.js
@@ -0,0 +1,50 @@
+const Usuario = require("../model/Usuario");
+const Empresa = require("../model/Empresa");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+module.exports = class authControllers {
+    static async userLogin(request, response){
+        const{
+            email,
+            senha,
+        } = request.body;
+
+        const user = await Usuario.findOne({ where: { email: email } })
+
+        const validateSenha = await bcrypt.compare(senha, user.senha)
+
+        if(!validateSenha){
+            response.status(422).json({ message: 'email ou senha invalidos!' })
+            return
+        }
+
+        const token = jwt.sign({
+            id: user.id
+        }, 'screet')
+
+        response.status(200).json({ token: token })
+    }
+
+    static async empresaLogin(request, response){
+        const{
+            email,
+            senha,
+        } = request.body;
+
+        const empresa = await Empresa.findOne({ where: { email: email } })
+
+        const validateSenha = await bcrypt.compare(senha, empresa.senha)
+
+        if(!validateSenha){
+            response.status(422).json({ message: 'email ou senha invalidos!' })
+            return
+        }
+
+        const token = jwt.sign({
+            id: empresa.id
+        }, 'screet')
+
+        response.status(200).json({ token: token })
+    }
+}
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 
+const authControllers = require('../controllers/authControllers')
 const empresaControllers = require('../controllers/empresaControllers')
 const usuarioControllers = require('../controllers/usuarioControllers')
 
@@ -8,5 +9,8 @@ router.get('/empresa', empresaControllers.getAllEmpresa)
 
 router.post('/usuario/create',usuarioControllers.creatUsuario)
 router.get('/usuario', usuarioControllers.getAllUsuario)
+
+router.post('/userlogin', authControllers.userLogin)
+router.post('/empresalogin', authControllers.empresaLogin)
 
 module.exports = router
