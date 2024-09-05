2e61604674ac63db1f1d30d1dd3e7809dba1f47a
LuizFloriano <luizfloriano92@gmail.com>
2024-09-05 16:45:35 -0300

    Creat pastas docker

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -2,8 +2,15 @@ const { Sequelize } = require('sequelize')
 require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql'
+    // host: 'localhost',
+    dialect: 'mysql',
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "root",
+    database: process.env.DB_NAME || "reconstrui"
+
+
 })
 
 try {
